43d28fee4880c2136649e9c361814df6d018d82b
Albert Mindrescu <albert-codrin.mindrescu@fmi.unibuc.ro>
2022-12-21 23:53:26 +0200

    [MOPS-3] Add Order Controller functionality

---
+++ src/main/java/org/example/hello/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package org.example.hello.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.example.hello.model.Order;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, String> {
+
+    public List<Order> findByClientId(String clientId);
+
+}
