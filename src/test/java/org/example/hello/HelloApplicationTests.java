14f2cca06ff87805ae5c53925e50a88254f9d315
Rares Mateiu <raresmateiu@yahoo.com>
2022-12-21 16:10:48 +0200

    [MOPS-2] Remove all files from old architecture

--- src/test/java/org/example/hello/HelloApplicationTests.java
+++ src/test/java/org/example/hello/HelloApplicationTests.java
@@ -3,7 +3,6 @@ package org.example.hello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.example.hello.data.InformationRepository;
 
 @SpringBootTest
 class HelloApplicationTests {
